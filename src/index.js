734a112f74cda6f6a3b2299e331dd2274271c4e8
Darya <daryagoryacheva94@gmail.com>
2023-02-08 01:20:31 +0300

    hashrouter

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import React from "react";
 import * as ReactDOMClient from "react-dom/client";
 import "./style.css";
 import App from "./components/App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOMClient.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
