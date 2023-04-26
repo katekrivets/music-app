19151bf3a804ba69de09acfe70f8c386f733f689
Ekaterina <katekrivets@me.com>
2023-04-25 17:32:26 -0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import { HashRouter } from "react-router-dom";
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
+console.log('test');
 
 root.render(
   <HashRouter>
